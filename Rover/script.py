52eae34a26dff1b216b5cef2e4491af2c8bbfeb5
codera21 <atul.nepal@gmail.com>
2019-05-11 17:04:19 +0545

    rover all done

--- Rover/script.py
+++ Rover/script.py
@@ -1,7 +1,11 @@
 import pandas as pd 
 df1 = pd.read_csv('Rover_1.csv')
 df2 = pd.read_csv('Rover_2.csv')
-df3 = pd.concat([df1, df2], axis=0, ignore_index=True)
-print(df3.count())
-df3 = df3.drop_duplicates()
-print(df3.count()) 
+df3 = pd.read_csv('Rover_3.csv')
+
+df = pd.concat([df1, df2, df3], axis=0, ignore_index=True)
+print(df.count())
+df = df.drop_duplicates()
+print(df.count()) 
+
+df.to_csv('rover_com.csv', index=False)
