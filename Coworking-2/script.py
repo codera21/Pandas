5d47f0842d6db17c7a517f28f2d97ffb94277903
codera21 <atul.nepal@gmail.com>
2019-05-20 15:04:03 +0545

    fixing, coworking :: performing str functions

---
+++ Coworking-2/script.py
@@ -0,0 +1,12 @@
+import pandas as pd
+df = pd.read_csv('coworker.csv', low_memory=False)
+
+
+for i, row in df.iterrows():
+    print("processing row " , i)
+    temp = df.loc[i, 'Address']
+    temp = temp.split(',')
+    temp = temp[-1]
+    df.loc[i, 'Country'] = temp
+
+df.to_csv('coworker-country_fix.csv' , index= False)
