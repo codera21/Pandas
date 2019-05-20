cf327db6fbc7fa45ab971e3693294717e8518dc7
codera21 <atul.nepal@gmail.com>
2019-05-20 12:42:40 +0545

    count of one col

--- mwdental/script.py
+++ mwdental/script.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 df = pd.read_csv('mw.csv')
-print(df.columns)
-print(df.count())
+# print(df.columns)
+print("SKU Count before ", df['SKU'].count())
 df1 = df.drop_duplicates(subset=['SKU'])
-print(df1.count())
+print("SKU Count After dedupe ", df['SKU'].count())
