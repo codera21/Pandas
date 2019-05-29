5a32e10dcff51aef47424edd84d4c51c47754b4a
codera21 <atul.nepal@gmail.com>
2019-05-29 13:20:44 +0545

    vericred qa step one added

---
+++ Vericred/valor_health/fns.py
@@ -0,0 +1,6 @@
+def get_specialities(specialities):
+    specialities_list = []
+    for speciality in specialities:
+        specialities_list.append(speciality['name'])
+
+    return specialities_list
---
+++ Vericred/valor_health/script.py
@@ -0,0 +1,22 @@
+import json
+from fns import *
+filepath = 'vericred_valor.json'
+
+speciality_count = 0
+speciality = set([])
+try:
+    fp = open(filepath)
+    for line in fp:
+        arr = json.loads(line)
+        specialities =get_specialities(arr['specialties'])
+        for i in specialities:
+            if(i == "Keith Poole, DO"):
+                print(line)
+                exit
+            speciality.add(i)
+finally:
+    fp.close()
+print(speciality)
+print("\n\n")
+print("Optometry" in speciality)
+
