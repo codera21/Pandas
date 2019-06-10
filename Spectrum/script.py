06ed3afe360addd412923e3bc2df76cd564fe8f0
codera21 <atul.nepal@gmail.com>
2019-06-10 23:52:29 +0545

    ..

--- Spectrum/script.py
+++ Spectrum/script.py
@@ -2,4 +2,5 @@ import pandas as pd
 
 df  = pd.read_csv('NSR_Cable_Pricing_Analysis - Data.csv')
 df1 = df.loc[df['Website'] == 'https://www.spectrum.com/internet.html']
-df.to_csv('Spectrum.csv' , index=False)
+df1  = df1.drop_duplicates()
+df1.to_csv('Spectrum.csv' , index=False)
