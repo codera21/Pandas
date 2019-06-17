e8d9d7677e47c6e89295252fb02efac51d5d540b
codera21 <atul.nepal@gmail.com>
2019-06-17 13:29:06 +0545

    ..:

--- MidwestDental/script.py
+++ MidwestDental/script.py
@@ -3,6 +3,10 @@ import pandas as pd
 df = pd.read_excel('midwestdental.xlsx')
 print(df.columns)
 df1 = df[df['Manufacturer'].isnull()]
-print(df1[['SKU',  'Product Page URL']].head())
+# select only a few rows
 df2  = df1[['SKU',  'Product Page URL']].head()
-df2.to_csv('test.csv')
+df2.to_csv('test_mfg.csv')
+
+df1 = df[df['MFR Part #'].isnull()]
+print(df1.count())
+df1.to_csv('test_part_num.csv' , index=False)
