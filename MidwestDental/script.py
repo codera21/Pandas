2be929a984e27d37c872e048e8bc891630b41e7f
codera21 <atul.nepal@gmail.com>
2019-06-17 12:38:53 +0545

    Mwdental

--- MidwestDental/script.py
+++ MidwestDental/script.py
@@ -4,3 +4,5 @@ df = pd.read_excel('midwestdental.xlsx')
 print(df.columns)
 df1 = df[df['Manufacturer'].isnull()]
 print(df1[['SKU',  'Product Page URL']].head())
+df2  = df1[['SKU',  'Product Page URL']].head()
+df2.to_csv('test.csv')
